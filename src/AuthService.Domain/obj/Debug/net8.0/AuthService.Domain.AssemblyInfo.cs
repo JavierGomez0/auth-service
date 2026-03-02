563c063988552afe25644e31a6f20d37ed8bb0ad
JavierGomez0 <jgomez-2024026@kinal.edu.gt>
2026-03-02 07:48:30 -0600

    se arreglaron las credenciales

--- src/AuthService.Domain/obj/Debug/net8.0/AuthService.Domain.AssemblyInfo.cs
+++ src/AuthService.Domain/obj/Debug/net8.0/AuthService.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bac2ade7f7dae1c61ba2141c184dfd8342592a6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffc36bd7fb7424e1d7f568d20a54ba96e972e76d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
